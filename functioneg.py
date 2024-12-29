93c5d1cc3198970d91256d890775890b27af7621
alija <alijarai414@gmail.com>
2024-12-29 15:14:19 +0545

    first commit

---
+++ functioneg.py
@@ -0,0 +1,7 @@
+print('this is a function example')
+
+def find_modulus(a,b):
+    modulus = a%b
+    print('The modulus of' + str(a) + ' % '  + str(b) + ' is ' + str(modulus))
+
+find_modulus(14,3)
